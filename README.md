46dffa41b1e4359fcc4ff47ffcc46d6bb41c9119
Won-Kyung Chung <wkchung@kth.se>
2019-10-08 16:28:07 +0200

    Migrate to gulp 4 and npm build by jenkins.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # KTH CKEditor build
 
-> __*Do not manually edit files in the `ckeditor` folder!*__
+> **_Do not manually edit files in the `ckeditor` folder!_**
 
-This project contains a custom build of the [CKEditor][CKEditor].
+This project contains a custom build of the [CKEditor][ckeditor].
 
-To update the CKEditor code, [go to the builder][CKEditorBuilder] and
+To update the CKEditor code, [go to the builder][ckeditorbuilder] and
 upload the `ckeditor/build-config.js` file to reuse the same settings
 and plugins. Download ZIP file, delete the current `ckeditor` folder,
 unpack the ZIP file into this repository, and finally commit and push.
@@ -16,20 +16,47 @@ When installing this project through npm, run the following command:
 npm install --save-dev @kth/kth-ckeditor-build
 ```
 
-In the module there are Gulp tasks which are automatically executed during post installation.
+In the module there is a gulp (version 4) task generator which can copy CKEditor files to the your project target directory.
 They move the plugin as well as a configuration file into your project directory.
+Here is an example how it can be used.
+
+```
+const ckGulp = require('@kth/kth-ckeditor-build/gulpfile')
+const ckEditorBuild = ckGulp.buildTask(
+  gulp,
+  './node_modules/@kth/kth-ckeditor-build',
+  './dist/js/ckeditor',
+  'profiles-web'
+)
+
+// if there is a vendor task, you can add it there
+gulp.task(
+  'vendor',
+  gulp.series([ckEditorBuild, vendor], done => {
+    done()
+  })
+)
+
+// or define as a standalone task which can  then added to the final build task
+gulp.task(
+  'ckEditorBuild',
+  gulp.series([ckEditorBuild], done => {
+    done()
+  })
+)
+```
 
 In order for the configuration file to be used, it has to be configured in the client.
-For example in your page view module. Attention: for Bootstrap formatting in the editor, 
+For example in your page view module. Attention: for Bootstrap formatting in the editor,
 provide a Bootstrap CSS path in the contentsCss array.
 This also a good place to override options that may be needed for your project
 
 ```javascript
-	CKEDITOR.replace('editor', {
-	  customConfig: 'customConfig.js',
-	  contentsCss: [ CKEDITOR.getUrl('contents.css'), 'path-to-bootstrap-css']
-	  // We need more height, lets override! height: 1337
-	})
+CKEDITOR.replace('editor', {
+  customConfig: 'customConfig.js',
+  contentsCss: [CKEDITOR.getUrl('contents.css'), 'path-to-bootstrap-css'],
+  // We need more height, lets override! height: 1337
+})
 ```
 
 Finally, reference the CKEditor JavaScript file `ckeditor/ckeditor.js`
@@ -45,14 +72,14 @@ is missing a way to edit the link text. Right now, the best way to
 ensure a specific link text is to first type out the text, then
 highlight it, then open the link dialog, enter a URL, and click OK. It
 is being worked on by the CKEditor team, but it's not certain when it's
-finished. 
+finished.
 
 ## Sample config
 
 For a sample config, check out the [server/public/js/index.js][sample]
 file.
 
-[CKEditor]: http://ckeditor.com
-[CKEditorBuilder]: http://ckeditor.com/builder
+[ckeditor]: http://ckeditor.com
+[ckeditorbuilder]: http://ckeditor.com/builder
 [local]: http://localhost:5050
 [sample]: server/public/js/index.js
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
 'use strict'
 const gulp = require('gulp')
+const mergeStream = require('merge-stream')
 
-gulp.task('ckeditor:moveCssOverrides', function () {
-  let sources = [ './node_modules/kth-ckeditor-build/cssOverrides/*' ]
-  return gulp.src(sources)
-    .pipe(gulp.destination('./public/css/'))
+gulp.task('ckeditor:moveCssOverrides', done => {
+  return gulp.src(['./node_modules/kth-ckeditor-build/cssOverrides/*']).pipe(gulp.destination('./public/css/'))
 })
-
-gulp.task('ckEditor:moveAssets', [ 'ckEditor:moveEditor', 'ckeditor:moveCssOverrides' ])
-
-gulp.task('ckEditor:moveCustomConfig', function () {
-  let sources = [ './node_modules/kth-ckeditor-build/customConfig/customConfig.js' ]
-  return gulp.src(sources)
+gulp.task('ckEditor:moveCustomConfig', done => {
+  return gulp
+    .src(['./node_modules/kth-ckeditor-build/customConfig/customConfig.js'])
     .pipe(gulp.dest('./public/js/ckeditor/'))
 })
 
-gulp.task('ckEditor:movePlugins', function () {
-  let sources = [ './node_modules/kth-ckeditor-build/plugins/**/*' ]
-  return gulp.src(sources)
-    .pipe(gulp.dest('./public/js/ckeditor/plugins/'))
+gulp.task('ckEditor:movePlugins', done => {
+  return gulp.src(['./node_modules/kth-ckeditor-build/plugins/**/*']).pipe(gulp.dest('./public/js/ckeditor/plugins/'))
 })
 
-gulp.task('ckEditor:moveEditor', [ 'ckEditor:movePlugins', 'ckEditor:moveCustomConfig' ], function () {
-  let sources = [ './node_modules/kth-ckeditor-build/ckeditor/**/*' ]
-  return gulp.src(sources)
-    .pipe(gulp.dest('./public/js/ckeditor/'))
-})
+gulp.task(
+  'ckEditor:moveEditor',
+  gulp.series(['ckEditor:movePlugins', 'ckEditor:moveCustomConfig'], done => {
+    return gulp.src(['./node_modules/kth-ckeditor-build/ckeditor/**/*']).pipe(gulp.dest('./public/js/ckeditor/'))
+  })
+)
+
+gulp.task('ckEditor:moveAssets', gulp.series(['ckEditor:moveEditor', 'ckeditor:moveCssOverrides'], done => {}))
 
 /**
  * Generates a gulp task function.
@@ -34,31 +31,62 @@ gulp.task('ckEditor:moveEditor', [ 'ckEditor:movePlugins', 'ckEditor:moveCustomC
  * @param {String} destination - the target directory for the output
  * @returns {Function}
  */
-function buildTask (gulp, base, destination) {
-  return function build () {
-    gulp.src(base + '/ckeditor/**/*')
-      .pipe(gulp.dest(destination))
-    gulp.src(base + '/plugins/**/*', { base: base })
-      .pipe(gulp.dest(destination))
-    gulp.src([
-      base + '/customConfig/*',
-      base + '/cssOverrides/*'
-    ])
-      .pipe(gulp.dest(destination))
-  }
+// function buildTask(gulp, base, destination) {
+//   return () => {
+//     gulp.src(base + '/ckeditor/**/*').pipe(gulp.dest(destination))
+//     gulp.src(base + '/plugins/**/*', { base: base }).pipe(gulp.dest(destination))
+//     gulp.src([base + '/customConfig/*', base + '/cssOverrides/*']).pipe(gulp.dest(destination))
+//   }
+// }
+
+const taskParams = []
+
+function buildCK(done) {
+  const tasks = taskParams.map(app => {
+    function buildByApp(done) {
+      return mergeStream(
+        gulp.src(app.base + '/ckeditor/**/*').pipe(gulp.dest(app.destination)),
+        gulp.src(app.base + '/plugins/**/*', { base: app.base }).pipe(gulp.dest(app.destination)),
+        gulp.src([app.base + '/customConfig/*', app.base + '/cssOverrides/*']).pipe(gulp.dest(app.destination))
+      )
+    }
+
+    buildByApp.displayName = `${app.name}_ckbuild`
+    return buildByApp
+  })
+
+  const ckTask = tasks[0]
+
+  return gulp.series(ckTask, seriesDone => {
+    seriesDone()
+    done()
+  })()
 }
 
-gulp.task('build', buildTask(gulp, '.', './dist/'))
-gulp.task('build:watch', function () {
-  return gulp.watch([
-    './plugins/**/*',
-    './customConfig/*',
-    './cssOverrides/*',
-    './ckeditor/**/*'
-  ], ['build'])
+function buildTask(gulp, base, destination, name = 'ckEditorBuild') {
+  taskParams.push({ name: name, base: base, destination: destination })
+  const build = gulp.series(buildCK)
+  return build
+}
+
+gulp.task('build', done => {
+  const base = '.'
+  const destination = './dist'
+  return (
+    gulp.src(base + '/ckeditor/**/*').pipe(gulp.dest(destination)),
+    gulp.src(base + '/plugins/**/*', { base: base }).pipe(gulp.dest(destination)),
+    gulp.src([base + '/customConfig/*', base + '/cssOverrides/*']).pipe(gulp.dest(destination))
+  )
+})
+
+gulp.task('build:watch', () => {
+  return gulp.watch(
+    ['./plugins/**/*', './customConfig/*', './cssOverrides/*', './ckeditor/**/*'],
+    gulp.parallel('build')
+  )
 })
 
 module.exports = {
   gulp: gulp,
-  buildTask: buildTask
+  buildTask: buildTask,
 }
