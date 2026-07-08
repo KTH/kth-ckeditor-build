68eb7b6c7a14d39035abd21076b9c3fdbd767165
Rickard Falk <ricf@kth.se>
2026-07-08 13:03:38 +0200

    chore(deps): bump eslint to v9

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,14 +1,14 @@
 /* global CKEDITOR */
 
-var query = new URLSearchParams(window.location.search)
-var lang = query.get('l') === 'en' ? 'en' : 'sv'
+const query = new URLSearchParams(window.location.search)
+const lang = query.get('l') === 'en' ? 'en' : 'sv'
 
-var editor = CKEDITOR.replace('editor', {
+const editor = CKEDITOR.replace('editor', {
   customConfig: CKEDITOR.getUrl('customConfig.js'),
   language: lang,
 })
 
-editor.on('loaded', function () {
+editor.on('loaded', () => {
   if (lang === 'sv' && editor.lang.link.displayText.toLowerCase() === 'display text') {
     // Missing Swedish translation for displayText.
     // Set it like this until it's provided by CKEditor.
